b291d76c8b3a4d0af26fcb34c9ffeef68e2c211d
Chris Gemignani <chris.gemignani@juiceanalytics.com>
2016-08-01 14:50:49 -0400

    Don't show .html on the end of a node link on the index page

--- pycco/generate_index.py
+++ pycco/generate_index.py
@@ -50,7 +50,11 @@ def generate_tree_html(tree):
     items = []
     for node, subtree in sorted(compat_items(tree)):
         if 'entry' in subtree:
-            html = u'<li><a href="{}">{}</a></li>'.format(subtree['entry']['relpath'], node)
+            node_text = node
+            node_parts = node_text.split('.')
+            if len(node_parts) == 3 and node_parts[-1] == 'html':
+                node_text = '.'.join(node_parts)
+            html = u'<li><a href="{}">{}</a></li>'.format(subtree['entry']['relpath'], node_text)
         else:
             html = u'<dl><dt>{}</dt><dd><ul>{}</ul></dd></dl>'.format(node, generate_tree_html(subtree))
 
